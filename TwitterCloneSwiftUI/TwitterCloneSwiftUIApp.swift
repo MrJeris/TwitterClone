104e6777d0b0f4c9cb4cd8584d6e5cb9804b33dc
MrJeris <mrjeris@yandex.ru>
2023-03-29 21:39:02 +0300

    Initial Commit

---
+++ TwitterCloneSwiftUI/TwitterCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneSwiftUIApp.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by MrJeris on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
